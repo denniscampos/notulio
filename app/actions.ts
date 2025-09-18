98df6c3b0727a0fd429b3b42b1064c186f606057
Dennis <dennisjr89@gmail.com>
2025-09-18 18:56:39 -0400

    chore: Test out openApi flashcard

--- app/actions.ts
+++ app/actions.ts
@@ -6,14 +6,22 @@ import { fetchMutation } from 'convex/nextjs';
 import { revalidatePath } from 'next/cache';
 
 export async function createArticleMetadata({ url = '' }: { url: string }) {
-  //TODO: Add ServerActionResult;
   // 1) Get scrape info
   const articleMetadata = await getArticleMetadata({
     url,
     formats: ['summary', 'markdown'],
   });
   // 2) OpenAI to digest information and create flashcards.
-  // await createFlashcards
+  const flashcards = await fetch('http://localhost:3000/api/flashcard', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: articleMetadata.title,
+      description: articleMetadata.description,
+      content: articleMetadata.body,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  console.log('flashcards: ', await flashcards.json());
   // 3) Send info to convex DB.
   try {
     const token = await getToken();
