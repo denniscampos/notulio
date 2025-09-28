b2e35e5317a47dadb05207a17bf8a3d108ca8b05
Dennis Campos <64917152+denniscampos@users.noreply.github.com>
2025-09-27 22:41:12 -0400

    chore: folder movement and auth check

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from 'react';
 import { ResetPasswordForm } from './reset-password-form';
 import { Loader2 } from 'lucide-react';
+import { getToken } from '@/lib/auth-server';
+import { redirect } from 'next/navigation';
 
 interface PageProps {
   searchParams: Promise<{ token?: string }>;
@@ -8,6 +10,10 @@ interface PageProps {
 
 export default async function ResetPasswordPage({ searchParams }: PageProps) {
   const token = (await searchParams).token;
+  const tokenAuth = await getToken();
+  if (tokenAuth) {
+    redirect('/articles');
+  }
   return (
     <Suspense fallback={<Loader2 className="animate-spin" />}>
       <ResetPasswordForm token={token} />
